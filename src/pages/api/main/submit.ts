3dec4baa70da5bab693a90754c6530d64fec36ee
Sebarina <xuling1987@gmail.com>
2025-03-14 10:11:58 +0800

    add edge support2

--- src/pages/api/main/submit.ts
+++ src/pages/api/main/submit.ts
@@ -1,4 +1,5 @@
 import { NextApiRequest, NextApiResponse } from 'next';
+export const runtime = 'edge';
 var db = {"id":{}}
 export default async function handler(req: NextApiRequest, res: NextApiResponse) {
     if (req.method !== 'POST') {
